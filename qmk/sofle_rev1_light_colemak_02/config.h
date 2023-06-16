7da4fc3ce24cb6378d01f5165dc3647e51148013
Joe Love <git@joelove.uk>
2023-06-16 09:38:28 +0100

    update sofle keymap

---
+++ qmk/sofle_rev1_light_colemak_02/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define EE_HANDS
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+#define RETRO_TAPPING
+#define WPM_LAUNCH_CONTROL
+#define SPLIT_WPM_ENABLE
+#define SPLIT_MODS_ENABLE
+#define SPLIT_LAYER_STATE_ENABLE
