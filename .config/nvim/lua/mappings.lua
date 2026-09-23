c28f84fd88b42cbb0a78d7556d74aa16e29d290c
Joe Love <git@joelove.dev>
2026-09-23 18:48:42 +0100

    ghostty,nvim: add cmd+n/cmd+shift+e shortcuts and skip quit confirmation

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -13,6 +13,14 @@ map("i", "jk", "<ESC>")
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "filetree toggle" })
 map("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "filetree find current file" })
 map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "filetree refresh" })
+map("n", "<A-E>", function()
+  local api = require "nvim-tree.api"
+  if api.tree.is_visible() then
+    api.tree.focus()
+  else
+    api.tree.toggle { focus = true }
+  end
+end, { desc = "filetree focus/open (cmd+shift+e)" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
@@ -20,6 +28,10 @@ map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "filetree refresh" }
 -- (macOS/Ghostty swallow cmd); tmux forwards the Alt sequence to the pane.
 map({ "n", "i", "v" }, "<A-p>", "<cmd>Telescope find_files<CR>", { desc = "find files (cmd+p)" })
 map({ "n", "i", "v" }, "<A-P>", "<cmd>Telescope commands<CR>", { desc = "command palette (cmd+shift+p)" })
+map("n", "<A-n>", function()
+  vim.cmd "enew"
+  vim.cmd "startinsert"
+end, { desc = "new blank buffer (cmd+n)" })
 map({ "n", "i", "v" }, "<A-s>", "<cmd>w<CR>", { desc = "save file (cmd+s)" })
 map(
   { "n", "i", "v" },
